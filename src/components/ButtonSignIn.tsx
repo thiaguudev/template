c25d58225d19024340914a3008973ea68440377e
Thiago Luiz Silva - Desenvolvimento <23242@cenl.cal>
2024-02-06 13:46:59 -0300

    add hotjar

--- src/components/ButtonSignIn.tsx
+++ src/components/ButtonSignIn.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import { useTranslations } from "next-intl";
 
 import { Button } from "@/components/ui/button";
-import { useTranslations } from "next-intl";
 
 export default function ButtonSignIn() {
   const t = useTranslations("Auth");
