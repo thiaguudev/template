0ded4ae698c8a902b0f12c30e79adf3518801bee
Thiago Luiz Silva - Desenvolvimento <23242@cenl.cal>
2024-03-06 15:45:21 -0300

    initial commit

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -11,7 +11,7 @@ export default function Testimonials() {
         <div className="py-12 md:py-20 border-t border-gray-800">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">Don't take our word for it</h2>
+            <h2 className="h2 mb-4">Dont take our word for it</h2>
             <p className="text-xl text-gray-400">
               Vitae aliquet nec ullamcorper sit amet risus nullam eget felis
               semper quis lectus nulla at volutpat diam ut venenatis tellus—in
@@ -46,9 +46,9 @@ export default function Testimonials() {
               </div>
               <blockquote className="text-lg text-gray-400 grow">
                 — Open PRO lets me quickly get the insights I care about so that
-                I can focus on my productive work. I've had Open PRO for about
-                24 hours now and I honestly don't know how I functioned without
-                it before.
+                I can focus on my productive work. Ive had Open PRO for about 24
+                hours now and I honestly dont know how I functioned without it
+                before.
               </blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
                 <cite className="text-gray-200 not-italic">Anastasia Dan</cite>{" "}
@@ -88,9 +88,9 @@ export default function Testimonials() {
               </div>
               <blockquote className="text-lg text-gray-400 grow">
                 — Open PRO lets me quickly get the insights I care about so that
-                I can focus on my productive work. I've had Open PRO for about
-                24 hours now and I honestly don't know how I functioned without
-                it before.
+                I can focus on my productive work. Ive had Open PRO for about 24
+                hours now and I honestly dont know how I functioned without it
+                before.
               </blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
                 <cite className="text-gray-200 not-italic">Anastasia Dan</cite>{" "}
@@ -130,9 +130,9 @@ export default function Testimonials() {
               </div>
               <blockquote className="text-lg text-gray-400 grow">
                 — Open PRO lets me quickly get the insights I care about so that
-                I can focus on my productive work. I've had Open PRO for about
-                24 hours now and I honestly don't know how I functioned without
-                it before.
+                I can focus on my productive work. Ive had Open PRO for about 24
+                hours now and I honestly dont know how I functioned without it
+                before.
               </blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
                 <cite className="text-gray-200 not-italic">Anastasia Dan</cite>{" "}
