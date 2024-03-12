6787b823fb71aa6ff28b14c1b6f2a6842253c850
Thiago Luiz Silva - Desenvolvimento <23242@cenl.cal>
2024-03-12 19:41:54 -0300

    initial commit

--- src/app/[locale]/app/surveys/page.tsx
+++ src/app/[locale]/app/surveys/page.tsx
@@ -2,19 +2,8 @@
 
 import { useState } from "react";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { AddButton } from "@/components/buttons";
 import { DialogSurvey } from "@/components/dialog-survey";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { Copy, Edit2, Link, Pause, Trash } from "iconsax-react";
 import CardSurvey from "@/components/card-survey";
 
 export default function SurveysPage() {
@@ -39,12 +28,23 @@ export default function SurveysPage() {
       </div>
       {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-5"> */}
       <div className="flex flex-col gap-5 mt-5">
-        <CardSurvey name="Survey #01" type="NPS" description="Survey NPS #01" />
-        <CardSurvey name="Survey #02" type="NPS" description="Survey NPS #02" />
+        <CardSurvey
+          name="Survey #01"
+          type="NPS"
+          description="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+          quantity={1689}
+        />
+        <CardSurvey
+          name="Survey #02"
+          type="NPS"
+          description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32."
+          quantity={0}
+        />
         <CardSurvey
           name="Survey #03"
           type="CSAT"
-          description="Survey CSAT #03"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing"
+          quantity={9}
         />
       </div>
     </div>
--- src/components/card-survey.tsx
+++ src/components/card-survey.tsx
@@ -15,12 +15,14 @@ type CardSurveyProps = {
   status?: number;
   type: "NPS" | "CES" | "CSAT" | "Custom";
   description?: string;
+  quantity: number;
 };
 
 export default function CardSurvey({
   name,
   type,
   description,
+  quantity,
   status,
 }: CardSurveyProps) {
   function getStatus(status: number) {
@@ -35,11 +37,18 @@ export default function CardSurvey({
   return (
     <Card className="group hover:bg-primary hover:text-white p-3">
       <CardHeader className="p-1">
-        <CardTitle className="flex justify-between items-center">
+        <CardTitle className="flex gap-3 items-center">
           {name} <Badge>{type}</Badge>
         </CardTitle>
-        <CardDescription>{description}</CardDescription>
+        <CardDescription className="flex justify-between mt-3 gap-3">
+          {description}
+          <div className="text-center min-w-28">
+            <span className="font-black text-2xl block">{quantity}</span>
+            responses
+          </div>
+        </CardDescription>
       </CardHeader>
+      {/* <CardContent></CardContent> */}
       <CardFooter className="flex gap-2 p-1">
         <Button variant="secondary" className="flex gap-1">
           <Edit2 size={16} />
