23d8bcc414b445463e239d2d99b71fecd21deee1
Thiago Luiz Silva - Desenvolvimento <23242@cenl.cal>
2024-03-14 16:37:25 -0300

    initial commit

--- src/app/[locale]/app/surveys/[surveyId]/page.tsx
+++ src/app/[locale]/app/surveys/[surveyId]/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { ResponsiveBar } from "@nivo/bar";
+
 export default function Survey({ params }: any) {
-  return <div className="mt-5">{JSON.stringify(params)}</div>;
+  const data = [
+    { category: "A", value: 10 },
+    { category: "B", value: 20 },
+    { category: "C", value: 15 },
+  ];
+
+  return (
+    <div className="mt-5">
+      <div className="w-28 h-20">
+        <ResponsiveBar data={data} legends={[]} />
+      </div>
+    </div>
+  );
 }
